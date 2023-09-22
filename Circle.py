c4f4c98b8f35eead98602cd4a861529cf789a1b5
Seungwoo Kang <swkang@gmail.com>
2023-09-22 15:47:16 +0900

    lec07 class

---
+++ Circle.py
@@ -0,0 +1,12 @@
+import math
+
+class Circle:
+    # Circle 객체를 생성한다.
+    def __init__(self, radius = 1):
+        self.radius = radius
+
+    def getPerimeter(self):
+        return 2 * self.radius * math.pi
+
+    def getArea(self):
+        return self.radius * self.radius * math.pi
